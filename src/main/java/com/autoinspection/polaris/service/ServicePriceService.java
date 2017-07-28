c655a99529d838fd5b99ed3877fa0b7f99810ec0
zhangfuqing <zhangfuqing@viewhigh.com>
2017-07-28 17:25:33 +0800

    wx improve

--- src/main/java/com/autoinspection/polaris/service/ServicePriceService.java
+++ src/main/java/com/autoinspection/polaris/service/ServicePriceService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.autoinspection.polaris.model.entity.ServicePriceEntity;
 
 public interface ServicePriceService {
+	
 	public List<ServicePriceEntity> listAllServicePrices();
 	public ServicePriceEntity getServicePriceById(int id);
 }
